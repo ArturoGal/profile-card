8b1177cc5ba400132d39eafed2486d19ca1dac60
Arturo Garcia Leon <arturo.l.garcia@oracle.com>
2023-09-10 22:35:53 -0600

    cypress - Add several automated tests for cards

--- src/profile-card/index.js
+++ src/profile-card/index.js
@@ -5,24 +5,22 @@ template.innerHTML = `
 <div class="profile-card">
   <p class="kit-number"></p>
   <img class="photo"/>
-  <div>
-    <p class="name"></p>
-    <div class="details">
-      <div class="info-slot">
-        <p class="position-label">Position: </p> <p class="position"> </p>
-      </div>
-      <div class="info-slot">
-        <p class="country-label">Country: </p> <p class="country"> </p>
-      </div>
-      <div class="info-slot">
-        <p class="age-label">Age: </p> <p class="age"> </p>
-      </div>
-      <div class="info-slot">
-        <p class="goals-label">Goals: </p> <p class="goals"> </p>
-      </div>
-      <div class="info-slot">
-        <p class="assists-label">Assists: </p> <p class="assists"> </p>
-      </div>
+  <p class="name"></p>
+  <div class="details">
+    <div class="info-slot">
+      <p class="position-label">Position: </p> <p class="position"> </p>
+    </div>
+    <div class="info-slot">
+      <p class="country-label">Country: </p> <p class="country"> </p>
+    </div>
+    <div class="info-slot">
+      <p class="age-label">Age: </p> <p class="age"> </p>
+    </div>
+    <div class="info-slot">
+      <p class="goals-label">Goals: </p> <p class="goals"> </p>
+    </div>
+    <div class="info-slot">
+      <p class="assists-label">Assists: </p> <p class="assists"> </p>
     </div>
   </div>
 </div>`;
